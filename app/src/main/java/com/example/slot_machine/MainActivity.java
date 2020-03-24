e7398388e9ae78c6209ee1f67e4c33319f5776ed
android@saruman.cs.widener.edu <android@saruman.cs.widener.edu>
2020-03-24 16:53:31 -0400

    point counter added

--- app/src/main/java/com/example/slot_machine/MainActivity.java
+++ app/src/main/java/com/example/slot_machine/MainActivity.java
@@ -23,12 +23,12 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imageView2;
     private ImageView imageView3;
     private ImageView imageView4;
-    private int slotpiclocation;
     private Random rand;
     public boolean on;
     public Handler handler;
     public updateCount update;
-    private int cherryLocation;
+    private int count;
+
 
 
     @Override
@@ -46,13 +46,13 @@ public class MainActivity extends AppCompatActivity {
         imageView3 =  findViewById(R.id.imageView3);
         imageView4 =  findViewById(R.id.imageView4);
 
-
+        //creating an new update method
         update = new updateCount();
-
         on = false;
-
+        count = 0;
+        //creating a new random method
         rand = new Random();
-        cherryLocation = rand.nextInt(3);
+        //creating a new handler
         handler = new Handler();
 
     }
@@ -62,12 +62,30 @@ public class MainActivity extends AppCompatActivity {
             on = false;
             //removes handler and stops counter
             handler.removeCallbacks(update);
+            if (imageView2.getDrawable() == imageView3.getDrawable()) {
+                count += 50;
+                pointCounter.setText(count + "");
+            }
+            else if (imageView3.getDrawable() == imageView4.getDrawable()) {
+                count += 50;
+                pointCounter.setText(count + "");
+            }
+            else if (imageView4.getDrawable() == imageView2.getDrawable()) {
+                count += 50;
+                pointCounter.setText(count + "");
+            }
+            else if (imageView2.getDrawable().equals(imageView3.getDrawable()) && imageView3.getDrawable().equals(imageView4.getDrawable()) &&imageView4.getDrawable().equals(imageView2.getDrawable())){
+                count += 50;
+                pointCounter.setText(count + "");
+            }
+
 
         } else {
             //else if its on keep and has not been clicked a second time counter keeps going
             on = true;
             //creates handler starts counter
-            handler.postDelayed(update, 1000);
+            handler.postDelayed(update, 100);
+
         }
 
     }
@@ -75,31 +93,31 @@ public class MainActivity extends AppCompatActivity {
 
         public void run(){
 
-            if (imageView2.getDrawable() == strawberryImage) {
-                imageView2.setImageDrawable(grapeImage);
-                } else if (imageView2.getDrawable() == grapeImage) {
-                imageView2.setImageDrawable(cherryImage);
-                } else {
+            if (imageView2.getDrawable() == grapeImage) {
                 imageView2.setImageDrawable(strawberryImage);
-                }
-            if (imageView3.getDrawable() == strawberryImage) {
-                imageView3.setImageDrawable(grapeImage);
-                } else if (imageView3.getDrawable() == grapeImage) {
-                imageView3.setImageDrawable(cherryImage);
+                } else if (imageView2.getDrawable() == strawberryImage) {
+                    imageView2.setImageDrawable(cherryImage);
                 } else {
-                imageView3.setImageDrawable(strawberryImage);
+                    imageView2.setImageDrawable(grapeImage);
+                }
+            if (imageView3.getDrawable() == cherryImage) {
+                    imageView3.setImageDrawable(grapeImage);
+                } else if (imageView3.getDrawable() == grapeImage) {
+                    imageView3.setImageDrawable(strawberryImage);
+                } else {
+                    imageView3.setImageDrawable(cherryImage);
                 }
             if (imageView4.getDrawable() == strawberryImage) {
-                imageView4.setImageDrawable(grapeImage);
+                    imageView4.setImageDrawable(grapeImage);
                 } else if (imageView4.getDrawable() == grapeImage) {
-                imageView4.setImageDrawable(cherryImage);
+                    imageView4.setImageDrawable(cherryImage);
                 } else {
-                imageView4.setImageDrawable(strawberryImage);
-            }
+                    imageView4.setImageDrawable(strawberryImage);
+                }
 
-            handler.postDelayed(update, 1000);
-
+            handler.postDelayed(update, 100);
 
         }
+
     }
 }
